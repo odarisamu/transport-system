f4aa4438c8af8ba7658a4ee86960084f6b843422
unknown <odarisamu@gmail.com>
2026-01-22 07:36:12 -0300

    Menu basic structure

--- src/main/java/com/projects/application/Main.java
+++ src/main/java/com/projects/application/Main.java
@@ -1,5 +1,44 @@
 package com.projects.application;
 
+import java.util.Scanner;
+
 public class Main {
-    menu principal + 3 menus auxiliares
+    public static void main(String[] args) {
+        System.out.println("Olá seja bem-vindo(a) ao sistema de fretes!");
+        Scanner input = new Scanner(System.in);
+        int number = 0;
+        do { 
+            System.out.println("##### Sistema de Fretes #####");
+            System.out.println("1. Fretes\n2. Veículos\n3. Motoristas\n4. Desligar");
+            System.out.print("Qual opção deseja? ");
+            number = Integer.parseInt(input.next( ));
+
+            switch(number){
+                case 1:
+                    freightAuxiliar( );
+                    break;
+                case 2:
+                    vehicleAuxiliar();
+                    break;
+                case 3:
+                    driverAuxiliar();
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.err.println("Entrada inválida! Tente novamente...");
+            }
+        } while (number != 4);
+        System.out.println("Encerrando o sistema...");
+    }
+
+    public static void freightAuxiliar( ){
+
+    }
+    public static void vehicleAuxiliar( ){
+        
+    }
+     public static void driverAuxiliar( ){
+        
+    }
 }
